ea56235904c500b17236d6b5a998ac5f3a370bd2
Malhar <malharbhuptani@gmail.com>
2022-04-14 20:10:31 +0530

    minheap with closure with only exposes add and remove

--- leets/medium/minHeap.js
+++ leets/medium/minHeap.js
@@ -1,60 +1,58 @@
 function findKthLargest(nums, k) {
   function MinHeap() {
-    this.items = [];
+    items = [];
+
+    const parent = function (idx) {
+      return idx % 2 === 0 ? (idx - 2) / 2 : (idx - 1) / 2;
+    };
+
+    const minChild = function (idx) {
+      let child1 = 2 * idx + 1;
+      let child2 = 2 * idx + 2;
+      if (!items[child2]) {
+        return child1;
+      }
+      return items[child1] < items[child2] ? child1 : child2;
+    };
+
+    const swap = function (idx1, idx2) {
+      [items[idx1], items[idx2]] = [items[idx2], items[idx1]];
+    };
+
+    bubbleUp = function () {
+      let idx = items.length - 1;
+      while (idx > 0) {
+        let parentIdx = parent(idx);
+        if (items[idx] >= items[parentIdx]) {
+          break;
+        }
+        swap(idx, parentIdx);
+        idx = parentIdx;
+      }
+    };
+
+    bubbleDown = function () {
+      let idx = 0;
+      while (idx < items.length >> 1) {
+        let minChildIdx = minChild(idx);
+        if (items[idx] <= items[minChildIdx]) {
+          break;
+        }
+        swap(idx, minChildIdx);
+        idx = minChildIdx;
+      }
+    };
   }
 
-  MinHeap.prototype.parent = function (idx) {
-    return idx % 2 === 0 ? (idx - 2) / 2 : (idx - 1) / 2;
+  MinHeap.prototype.add = function(ele) {
+    items.push(ele);
+    bubbleUp();
   };
 
-  MinHeap.prototype.minChild = function (idx) {
-    let child1 = 2 * idx + 1;
-    let child2 = 2 * idx + 2;
-    if (!this.items[child2]) {
-      return child1;
-    }
-    return this.items[child1] < this.items[child2] ? child1 : child2;
-  };
-
-  MinHeap.prototype.swap = function (idx1, idx2) {
-    [this.items[idx1], this.items[idx2]] = [this.items[idx2], this.items[idx1]];
-  };
-
-  MinHeap.prototype.bubbleUp = function () {
-    let idx = this.items.length - 1;
-
-    while (idx > 0) {
-      let parentIdx = this.parent(idx);
-      if (this.items[idx] >= this.items[parentIdx]) {
-        break;
-      }
-      this.swap(idx, parentIdx);
-      idx = parentIdx;
-    }
-  };
-
-  MinHeap.prototype.bubbleDown = function () {
-    let idx = 0;
-
-    while (idx < this.items.length >> 1) {
-      let minChildIdx = this.minChild(idx);
-      if (this.items[idx] <= this.items[minChildIdx]) {
-        break;
-      }
-      this.swap(idx, minChildIdx);
-      idx = minChildIdx;
-    }
-  };
-
-  MinHeap.prototype.add = function (ele) {
-    this.items.push(ele);
-    this.bubbleUp();
-  };
-
-  MinHeap.prototype.remove = function () {
-    let min = this.items.shift();
-    this.items.unshift(this.items.pop());
-    this.bubbleDown();
+  MinHeap.prototype.remove = function() {
+    let min = items.shift();
+    items.unshift(items.pop());
+    bubbleDown();
     return min;
   };
 
@@ -65,7 +63,6 @@ function findKthLargest(nums, k) {
       minHeap.remove();
     }
   });
-  console.log(minHeap);
   return minHeap.remove();
 }
 
